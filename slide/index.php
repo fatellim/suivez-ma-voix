498abf5b546ec9792f31bdabd8bdba1d8ece7e9e
fatellim <marcalban@live.fr>
2016-12-06 00:38:21 +0100

    Ajout template v1.0

---
+++ slide/index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html>
+<head>
+	<meta  charset="utf-8" />
+	<title>Wf3 | Suivez ma voix</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0" />
+</head>
+
+<body>
+
+	<!-- *****************************
+
+
+	 		Bar de navigation lateral
+
+
+	 ********************************** -->
+
+		<aside>
+				<img src="img/logoWf3.png">
+
+			<nav>
+				<ul>
+					<li class="color">Menu</li>
+					<li><a href="#">ACCEUIL</a></li>
+					<li><a href="#">A PROPOS</a></li>
+					<li><a href="#">Les ARTISTES</a></li>
+					<li><a href="#">LES EVENEMENTS DU JOUR</a></li>
+					<li><a href="#">NOUS CONTACTER</a></li>
+				</ul>
+			</nav>			
+		</aside>
+
+
+	<!-- *****************************
+
+
+	 	Partie de la Vue de l'acceuil du site
+
+
+	 ********************************** -->
+
+	 		
+		<!-- Contenu des vues  -->
+
+		<header>
+			<span id="titre">
+				<h1>SUIVEZ MA VOIX !</h1>
+					<h2>Prase d'acroche</h2>				
+			</span>
+
+			<span id="connexion">
+
+				<a href="connexion.php" >Connexion</a>
+
+				<a href="inscription.php" >Inscription</a>
+
+			</span>
+
+		</header>
+
+		<main>
+	 			<!-- Section haut de page (Image plus titre) -->
+
+
+	 		<!-- Artiste Image plus description   -->
+	 		<section>
+
+		 		<span class="imgSec1">
+
+		 					<img src="img/default.jpg" alt=""  title="image Artistes" >
+		 					<p class="desc">description image 1</p>
+		 		</span>
+
+		 		<span class="imgSec1">
+
+		 					<img src="img/default.jpg" alt=""  title="image Artistes" >
+		 					<p class="desc">description image 2</p>
+		 		</span>
+
+		 		<span class="imgSec1">
+
+		 					<img src="img/default.jpg" alt=""  title="image Artistes" >
+		 					<p class="desc">description image 3</p>
+		 		</span>
+
+		 		<span class="imgSec1">
+
+		 					<img src="img/default.jpg" alt=""  title="image Artistes" >
+		 					<p class="desc">description image 4</p>
+		 		</span>
+
+
+	 		</section>
+
+		</main>
+
+
+		<!-- *****************************
+
+
+		 	Fin Vue de l'acceuil du site
+
+
+		 ********************************** -->
+
+	
+	
+
+</body>
+</html>
